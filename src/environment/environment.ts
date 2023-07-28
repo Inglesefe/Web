cb081c6acdd6cc56f143e73e551deeec90052309
Leandro Baena Torres <leandrobaena@paradigma.com.co>
2023-07-27 19:21:44 -0500

    api con https

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -4,5 +4,5 @@
 export const environment = {
   AES_KEY: 'AesKey*Golden$23',
   AES_IV: 'AesIV*Golden2023',
-  API_AUTH_URL: 'http://localhost:30000/'
+  API_AUTH_URL: 'https://localhost:30001/'
 };
